a195669e03cf35b7519c490edcc3a3414c69d4d8
meferemo <64318526+meferemo@users.noreply.github.com>
2020-06-03 11:00:12 +0100

    Create meferemo.py

---
+++ Scripts/meferemo.py
@@ -0,0 +1,11 @@
+def outputScript():
+	"""
+	A fucntion that outputs text of my details for HNG Internship
+	"""
+	fullName = 'Michael Eferemo'
+	ID = 'HNG -05538'
+	language = 'python'
+
+	print(f'Hello World, this is {fullName} with HNGi7 ID {ID} using {language} for stage 2 task')
+
+outputScript()	
